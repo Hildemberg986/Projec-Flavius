64df5a1545f9514bd752917bbf88e1193884ae54
ehaisa <isalaura1304@gmail.com>
2023-06-11 17:54:55 -0300

    creat project #5

---
+++ Quest.3/Quest3.py
@@ -0,0 +1,11 @@
+palavra = input("Escolha uma palavra: ")
+palavra = palavra.upper()
+dicio = {}
+for letra in palavra:
+    if letra not in dicio:
+        dicio[letra] = 1
+    else:
+        dicio[letra] += 1
+elementos = ["%s = %sx" % (letra, quantidade) for letra, quantidade in dicio.items()]
+resultado = ", ".join(elementos)
+print("A palavra %s possui: %s" % (palavra, resultado))
