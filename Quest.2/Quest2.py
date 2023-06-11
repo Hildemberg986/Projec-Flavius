c091b92635d7c365b2fcc53f022cbd591b7dfb46
ehaisa <isalaura1304@gmail.com>
2023-06-11 18:17:51 -0300

    modified code #3

--- Quest.2/Quest2.py
+++ Quest.2/Quest2.py
@@ -2,12 +2,15 @@ print("Codificando palavras")
 palavra = input("O que você quer codificar? ")
 letrasCodificadas = []
 for letra in palavra:
-    valorPalavra = ord(letra)
-    valorSucessor = valorPalavra + 1
-    if letra == 'Z':
-        letraCodificada = 'A'
+    if letra == ' ':      # Modificação feita para manter o espaço intacto ao codificar frases, antes estava sendo transformado em '!'
+        letrasCodificadas.append(letra)
     else:
-        letraCodificada = chr(valorSucessor)
+        valorPalavra = ord(letra)
+        valorSucessor = valorPalavra + 1
+        if letra == 'Z':
+            letraCodificada = 'A'
+        else:
+            letraCodificada = chr(valorSucessor)
     letrasCodificadas.append(letraCodificada)
 print("Codificação feita")
 print("Resultado: ", "".join(letrasCodificadas))
