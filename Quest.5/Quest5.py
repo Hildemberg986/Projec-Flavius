460339075796b68b55cc2aed14e10744fbef37b4
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-12 13:11:49 -0300

    update creating the clear screen definition #6

--- Quest.5/Quest5.py
+++ Quest.5/Quest5.py
@@ -3,52 +3,45 @@ import platform
 
 system = platform.system()
 
+def limpar_tela():
+    if system == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+        
 nome = input("Digite seu primeiro nome: ")
 while nome.isalpha() == False:
   print()
   print("O nome é inválido pois possui caracteres especiais. Por favor, digite novamente.")
   print()
   nome = input("Digite seu primeiro nome: ")
-if system == 'Windows':
-  os.system('cls')
-else:
-  os.system('clear')
+limpar_tela()
+
 snome = input("Digite seu segundo nome: ")
 while snome.isalpha() == False:
   print()
   print("O nome é inválido pois possui caracteres especiais. Por favor, digite novamente.")
   print()
   snome = input("Digite seu segundo nome: ")
-if system == 'Windows':
-  os.system('cls')
-else:
-  os.system('clear')
+limpar_tela()
+
 email = input("Digite seu email: ")
 while True:
   if email.count("@") == 1 and email.count(".") >= 1:
     arroba = email.index("@")
     ponto = email.index(".")
     if arroba > 0 and arroba < len(email) - 1 and ponto > (arroba + 1) and len(email) - ponto >= 3:
-      if system == 'Windows':
-        os.system('cls')
-      else:
-        os.system('clear')
+      limpar_tela()
       break
     else:
       print("O e-mail é inválido. Por favor, digite novamente.")
       print()
       email = input("Digite seu email: ")
-    if system == 'Windows':
-      os.system('cls')
-    else:
-      os.system('clear')  
+    limpar_tela() 
   else:
     print("O e-mail é inválido. Por favor, digite novamente.")
     print()
     email = input("Digite seu email: ")
-  if system == 'Windows':
-    os.system('cls')
-  else:
-    os.system('clear')  
+  limpar_tela()
 print("Seu nome é: %s %s"%(nome, snome))
 print("Seu email é:", email)
