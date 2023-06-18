43062b29e8c8eb26fa931989ac6ede0abdb6bc57
ehaisa <isalaura1304@gmail.com>
2023-06-17 21:17:28 -0300

    putting notes #9

--- Quest.3/Quest3.py
+++ Quest.3/Quest3.py
@@ -1,4 +1,26 @@
-print("Contador de letras!")
+## Questão no. 3 da lista
+## Desenvolvido por: Isa Laura e Luiz Miguel
+## Revisado e testado por: Hildemberg Eling
+
+## PRIMEIRA VERSÃO DO CÓDIGO:
+
+palavra = input("Escolha uma palavra: ")
+palavra = palavra.upper()
+dicio = {}
+for letra in palavra:
+    if letra not in dicio:
+        dicio[letra] = 1
+    else:
+        dicio[letra] += 1
+elementos = ["%s = %sx" % (letra, quantidade) for letra, quantidade in dicio.items()]
+resultado = ", ".join(elementos)
+print("A palavra %s possui: %s" % (palavra, resultado))
+
+########################################################
+
+## SEGUNDA VERSÃO:
+
+print("Contador de letras!") # Adição apenas estética
 palavra = input("Escolha uma palavra: ")
 palavra = palavra.upper()
 dicio = {}
